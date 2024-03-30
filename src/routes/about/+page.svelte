cab68b6aa427eaacace3192c9191d63ed04fd087
Yashodhan Joshi <yjdoc2@gmail.com>
2024-03-30 14:38:37 +0530

    Commit incomplete about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<div
+	class="border-token border-surface-500-400-token rounded-token bg-surface-200-700-token w-4/5 mx-auto p-5 text-center"
+>
+	<h1>About</h1>
+    <hr />
+	<br />
+	<p class="text-xl">
+		I'm a curious person with interest in low level systems such as operating systems, compilers and other system-software.
+		I like to learn new things, and help others learn things if I can. I try to focus on maintainability and debuggablity when I write code,
+		because I like both of them! I also like to create documentation - both for developers and users - as I go along ; because we are going to forget 
+		why we added that line of code six months later ;)
+	</p>
+	<p class="text-xl">
+		Apart from that, I am also fascinated by physics , specifically the General Theory of Relativity. I feel it's a really beautiful theory, and one day
+		I hope to learn enough maths to actually understand it :) That said, I do think that Sets are one of the most profound things in maths. starting with
+		almost nothing and then building almost everything from that. Come to think of it, I like maths as well.
+	</p>
+	<p class="text-xl">
+		I like reading, mostly for fun. Some of my favorites are<br />
+		- <b>Faster Fene</b> series by <b>Bha. Ra. Bhagvat</b> <br/>
+		- Marathi version of <b>Jules Verne , Translated</b> by <b>Bha. Ra. Bhagvat</b> <br />
+		- <b>Discowrld</b> Series by <b>Sir Prof. Terry Pratchett</b> <br />
+		To list a few. If you are a booklover, you know how hard it is to list only three! I am going to list more at TODO, so feel free to check that out.  
+	</p>
+</div>
+
+<style>
+	p {
+		text-align: justify;
+		margin-bottom: 1rem;
+	}
+	b{
+		color: brown;
+	}
+</style>
