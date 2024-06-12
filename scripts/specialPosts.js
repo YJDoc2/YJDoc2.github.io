62a545be568d6fcffc10ec864e2ed81fcb66973b
Yashodhan Joshi <yjdoc2@gmail.com>
2024-06-12 21:38:13 +0530

    Add note and head tag for logrocket posts

--- scripts/specialPosts.js
+++ scripts/specialPosts.js
@@ -3,9 +3,11 @@
 // For example, series posts should have a info abot series injected,
 // LogRocket posts should havea disclaimer injected etc.
 
+
+// Inject 'this post is part of the series...' for series posts
 hexo.extend.filter.register("before_post_render", function (data) {
   // only deal with posts
-  if (data.layout !== "post") {
+  if (data.layout !== "post" || !data.series) {
     return undefined;
   }
   if (data.series && data.categories.length < 1) {
@@ -20,6 +22,39 @@ hexo.extend.filter.register("before_post_render", function (data) {
   const category = data.categories.data[0].name;
   data.content =
     `_This post is part of a series called **${series}** . You can find other posts [here](/categories/${category})._\n\n` +
+    data.content;
+
+  return data;
+});
+
+// Inject 'this post was originally published...' for LogRocket posts
+hexo.extend.filter.register("before_post_render", function (data) {
+  // only deal with posts
+  if (data.layout !== "post" || !data.logrocket) {
+    return undefined;
+  }
+  if (data.logrocket && data.categories.length < 1) {
+    console.error(
+      `logrocket is specified, but no category is found in ${data.source}`,
+    );
+    throw new Error(
+      `logrocket is specified, but no category is found in ${data.source}`,
+    );
+  }
+  const logrocketLink = data.logrocket;
+  const category = data.categories.data[0].name;
+
+  if(category !== 'logrocket'){
+    console.error(
+      `logrocket is specified, but first category is not 'logrocket' in ${data.source}`,
+    );
+    throw new Error(
+      `series is specified, but first category is not 'logrocket' in ${data.source}`,
+    );
+  }
+
+  data.content =
+    `_This post was originally posted on LogRocket blog. You can see it [here](${logrocketLink})._\n\n` +
     data.content;
 
   return data;
