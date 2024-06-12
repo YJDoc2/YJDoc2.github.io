8547624b8ff9c40c342ac686ce7af8d8c1a623d3
Yashodhan Joshi <yjdoc2@gmail.com>
2024-06-12 12:56:15 +0530

    Add tag validation script

---
+++ scripts/tagValidation.js
@@ -0,0 +1,16 @@
+const validTags = ['rust'];
+
+hexo.extend.filter.register('before_post_render', function(data){
+  if(data.tags.data.length > 0 &&data.tags.data[0].name){
+    const tags = data.tags.data[0].name.split(',');
+    const src = data.source;
+    for(const tag of tags){
+      if (!validTags.includes(tag)){
+        console.error(`Invalid tag '${tag}' found in ${src}`);
+        throw new Error(`invalid tag '${tag}' found in ${src}`);
+      }
+    }
+  }
+  // we don't want to change data
+  return undefined;
+});
